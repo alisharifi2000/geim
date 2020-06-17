e54488abb1ea8a9ee60d8fdc59278225821e0903
Sk7w4tch3r <32716448+Sk7w4tch3r@users.noreply.github.com>
2020-06-17 17:23:41 +0430

    template ready to code moving cars

---
+++ Assets/destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class destr : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Vector3 boundary = new Vector3(0,0,0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
